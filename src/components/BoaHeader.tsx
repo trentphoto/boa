ae551b11e7fb7a97f226092e9300d876f13262b9
James Trent <jamestrent@jamess-imac-pro.uls.edu>
2019-02-01 09:46:33 -0500

    updated BoaHeader

--- src/components/BoaHeader.tsx
+++ src/components/BoaHeader.tsx
@@ -7,12 +7,8 @@ const BoaHeader = () => (
     <div className="container">
       <div className="row">
         <div className="col mr-auto">
-          <h1 className="main-header__title text-white">
-            Building One Another
-          </h1>
-          <p className="main-header__subtitle text-white">
-            A Podcast About Encouragement
-          </p>
+          <h1 className="BoaHeader__title">Building One Another</h1>
+          <p className="BoaHeader__subtitle">A Podcast About Encouragement</p>
         </div>
         <div className="col ml-auto" />
       </div>
